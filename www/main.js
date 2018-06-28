aa3cc47610b0e9c1177506a06ae0b8add5fcc59a
Jordan Goulder <jordan.goulder@gmail.com>
2018-06-28 15:20:22 -0400

    Updated based on ESLint recommendatios

--- www/main.js
+++ www/main.js
@@ -1,91 +1,87 @@
-window.addEventListener('load', function (event) {
-
-    // Get references to all the DOM elements
-    var dom = {
-        wordList:               document.getElementById('word-list'),
-        chosenWords:            document.getElementById('chosen-words'),
-        anotherButton:          document.getElementById('another'),
-        wordListTextArea:       document.getElementById('word-list-edit'),
-        cancelEditWordsButton:  document.getElementById('cancel-edit-words'),
-        saveWordsButton:        document.getElementById('save-words'),
-        editWordsButton:        document.getElementById('edit-words'),
-        defaultWordsButton:     document.getElementById('default-words'),
-        listEditControls:       document.getElementById('list-edit-controls'),
-        listDisplayControls:    document.getElementById('list-display-controls')
-    };
-
-    // Update the word list
-    function updateWordList()
-    {
-        // Remove all the items from the word list
-        while (dom.wordList.hasChildNodes()) {
-            dom.wordList.removeChild(dom.wordList.lastChild);
-        }
-
-        // Add each of the words to the list
-        WordBank.words().forEach(function (word) {
-            var wordTextNode = document.createTextNode(word);
-            var wordListItem = document.createElement("li");
-            wordListItem.appendChild(wordTextNode);
-            dom.wordList.appendChild(wordListItem);
-        });
-    }
-
-    // Update the chosen words
-    function updateChosenWords()
-    {
-        var chosenWords = WordBank.chooseRandomWords();
-
-        if (chosenWords.length > 1) {
-            // Display the chosen words
-            dom.chosenWords.innerHTML = chosenWords.join(' ');
-        }
-        else {
-            // Tell the user they need more words
-            dom.chosenWords.innerHTML = 'Not enough words to choose from!';
-        }
-    }
-
-    // Choose a new set of words when the user asks nicely
-    dom.anotherButton.addEventListener('click', function (event) {
-        updateChosenWords();
-    });
-
-    // User wants to cancel editing word list
-    dom.cancelEditWordsButton.addEventListener('click', function (event) {
-        dom.listDisplayControls.classList.remove('hidden');
-        dom.listEditControls.classList.add('hidden');
-        dom.anotherButton.disabled = false;
-    });
-
-    // Users wants to use the default word list
-    dom.defaultWordsButton.addEventListener('click', function (event) {
-        dom.wordListTextArea.value = WordBank.defaultWords().join('\n');
-    });
-
-    // User wants to save the word list
-    dom.saveWordsButton.addEventListener('click', function (event) {
-        dom.listDisplayControls.classList.remove('hidden');
-        dom.listEditControls.classList.add('hidden');
-        dom.anotherButton.disabled = false;
-
-        WordBank.setWords(dom.wordListTextArea.value.split('\n'));
-
-        updateWordList();
-    });
-
-    // User wants to edit the word list
-    dom.editWordsButton.addEventListener('click', function(event) {
-        dom.listDisplayControls.classList.add('hidden');
-        dom.listEditControls.classList.remove('hidden');
-        dom.anotherButton.disabled = true;
-
-        dom.wordListTextArea.value = WordBank.words().join('\n');
-    });
+window.addEventListener('load', function windowLoad() {
+  // Get references to all the DOM elements
+  var dom = {
+    wordList: document.getElementById('word-list'),
+    chosenWords: document.getElementById('chosen-words'),
+    anotherButton: document.getElementById('another'),
+    wordListTextArea: document.getElementById('word-list-edit'),
+    cancelEditWordsButton: document.getElementById('cancel-edit-words'),
+    saveWordsButton: document.getElementById('save-words'),
+    editWordsButton: document.getElementById('edit-words'),
+    defaultWordsButton: document.getElementById('default-words'),
+    listEditControls: document.getElementById('list-edit-controls'),
+    listDisplayControls: document.getElementById('list-display-controls')
+  };
 
     // Update the word list
-    updateWordList();
+  function updateWordList() {
+    // Remove all the items from the word list
+    while (dom.wordList.hasChildNodes()) {
+      dom.wordList.removeChild(dom.wordList.lastChild);
+    }
 
-    // Update the chosen words
+    // Add each of the words to the list
+    window.WordBank.words().forEach(function addWordToList(word) {
+      var wordTextNode = document.createTextNode(word);
+      var wordListItem = document.createElement('li');
+      wordListItem.appendChild(wordTextNode);
+      dom.wordList.appendChild(wordListItem);
+    });
+  }
+
+  // Update the chosen words
+  function updateChosenWords() {
+    var chosenWords = window.WordBank.chooseRandomWords();
+
+    if (chosenWords.length > 1) {
+      // Display the chosen words
+      dom.chosenWords.innerHTML = chosenWords.join(' ');
+    } else {
+      // Tell the user they need more words
+      dom.chosenWords.innerHTML = 'Not enough words to choose from!';
+    }
+  }
+
+  // Choose a new set of words when the user asks nicely
+  dom.anotherButton.addEventListener('click', function anotherButtonClick() {
     updateChosenWords();
+  });
+
+  // User wants to cancel editing word list
+  dom.cancelEditWordsButton.addEventListener('click', function cancelEditWordsButtonClick() {
+    dom.listDisplayControls.classList.remove('hidden');
+    dom.listEditControls.classList.add('hidden');
+    dom.anotherButton.disabled = false;
+  });
+
+  // Users wants to use the default word list
+  dom.defaultWordsButton.addEventListener('click', function defaultWordsButtonClick() {
+    dom.wordListTextArea.value = window.WordBank.defaultWords().join('\n');
+  });
+
+  // User wants to save the word list
+  dom.saveWordsButton.addEventListener('click', function saveWordsButtonClick() {
+    dom.listDisplayControls.classList.remove('hidden');
+    dom.listEditControls.classList.add('hidden');
+    dom.anotherButton.disabled = false;
+
+    window.WordBank.setWords(dom.wordListTextArea.value.split('\n'));
+
+    updateWordList();
+  });
+
+  // User wants to edit the word list
+  dom.editWordsButton.addEventListener('click', function editWordsButtonClick() {
+    dom.listDisplayControls.classList.add('hidden');
+    dom.listEditControls.classList.remove('hidden');
+    dom.anotherButton.disabled = true;
+
+    dom.wordListTextArea.value = window.WordBank.words().join('\n');
+  });
+
+  // Update the word list
+  updateWordList();
+
+  // Update the chosen words
+  updateChosenWords();
 });
--- www/storage.js
+++ www/storage.js
@@ -1,68 +1,63 @@
-var Storage = (function (undefinded) {
+window.Storage = (function Storage(undefinded) {
+  // Checks to see of storage is supported and available
+  // From https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+  var _storageAvailable = function _storageAvailable(type) {
+    try {
+      var storage = window[type];
+      var x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return e instanceof DOMException &&
+              (
+                // everything except Firefox
+                e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === 'QuotaExceededEror' ||
+                // Firefox
+                e.name === 'NS_ERROR_DOM_QUOTA_REACHED'
+              ) &&
+              // acknowledge QuoteExceededError only if there's something already stored
+              storage.length !== 0;
+    }
+  };
 
-    // Checks to see of storage is supported and available
-    // From https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
-    var _storageAvailable = function (type)
-    {
-        try {
-            var storage = window[type], x = '__storage_test__';
-            storage.setItem(x, x);
-            storage.removeItem(x);
-            return true;
-        }
-        catch (e) {
-            return e instanceof DOMException &&
-                (
-                    // everything except Firefox
-                    e.code === 22 ||
-                    // Firefox
-                    e.code === 1014 ||
-                    // test name field too, because code might not be present
-                    // everything except Firefox
-                    e.name === 'QuotaExceededEror' ||
-                    // Firefox
-                    e.name === 'NS_ERROR_DOM_QUOTA_REACHED'
-                ) &&
-                // acknowledge QuoteExceededError only if there's something already stored
-                storage.length !== 0;
-        }
-    };
+  // Sets value of the data stored at the key if storage is available
+  var set = function set(key, value) {
+    if (_storageAvailable('localStorage')) {
+      localStorage.setItem(key, JSON.stringify(value));
+      return true;
+    }
+    return false;
+  };
 
-    // Sets value of the data stored at the key if storage is available
-    var set = function (key, value) {
-        if (_storageAvailable('localStorage')) {
-            localStorage.setItem(key, JSON.stringify(value));
-            return true;
-        }
-        else {
-            return false;
-        }
-    };
+  // Gets the value of the data stored at key if storage is available
+  // If the value of the key cannot be retrieved and a default is specified, the default will be used
+  // If the storeDefault value is true then the default will also be stored at the key
+  var get = function get(key, defaultValue, storeDefault) {
+    var value = null;
 
-    // Gets the value of the data stored at key if storage is available
-    // If the value of the key cannot be retrieved and a default is specified, the default will be used
-    // If the storeDefault value is true then the default will also be stored at the key
-    var get = function (key, defaultValue, storeDefault) {
-        var value = null;
+    if (_storageAvailable('localStorage')) {
+      value = JSON.parse(window.localStorage.getItem(key));
+    }
 
-        if (_storageAvailable('localStorage')) {
-            value = JSON.parse(window.localStorage.getItem(key));
-        }
+    if (value === null && defaultValue !== undefinded) {
+      value = defaultValue;
 
-        if (value === null && defaultValue !== undefinded) {
-            value = defaultValue;
+      if (!!storeDefault) {
+        set(key, value);
+      }
+    }
 
-            if (!!storeDefault) {
-                set(key, value);
-            }
-        }
+    return value;
+  };
 
-        return value;
-    };
-
-    return {
-        set: set,
-        get: get,
-    };
-
+  return {
+    set: set,
+    get: get
+  };
 })();
